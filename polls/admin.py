8d28ca645e31199ea0d9bb54cf8f1c3994f00684
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-04-30 03:59:07 +0100

    feat:customizing Django’s automatically-generated admin site

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from .models import Choice, Question
 
-# Register your models here.
-from .models import Question
+# Inline for Choices in the Question admin
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
+# Admin customization for Question model
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+# Register the admin class with the Question model
+admin.site.register(Question, QuestionAdmin)
