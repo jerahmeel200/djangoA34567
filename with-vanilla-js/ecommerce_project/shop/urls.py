90786ed731bbd06a98205910063b3329f3e1977f
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-05-12 16:32:40 +0100

    feat: implement url

---
+++ with-vanilla-js/ecommerce_project/shop/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, CartViewSet
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'carts', CartViewSet, basename='cart')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
