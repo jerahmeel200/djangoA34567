abfe061bd90c0026d53d67c0d388e32af93970f6
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-05-21 23:43:28 +0100

    feat: products

--- geekForGeek/project/urls.py
+++ geekForGeek/project/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
-    # path('api/products/', include('products.urls')),
+    path('api/products/', include('products.urls')),
     # path('api/orders/', include('orders.urls')),
     # path('api/cart/', include('cart.urls')),
     # path('api/payments/', include('payments.urls')),
