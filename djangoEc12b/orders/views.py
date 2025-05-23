e3ddf236f75b767cf7d36440c904f566f3c294db
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-05-23 20:11:54 +0100

    feat:Implement Orders views and migrations

--- djangoEc12b/orders/views.py
+++ djangoEc12b/orders/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Order, OrderItem
+from cart.cart import Cart
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+
+@login_required
+def create_order(request):
+    cart = Cart(request)
+    order = Order.objects.create(user=request.user)
+    for item in cart:
+        OrderItem.objects.create(
+            order=order,
+            product=item['product'],
+            price=item['product'].price,
+            quantity=item['quantity']
+        )
+    cart.clear()
+    return redirect('order_list')  
+
+@login_required
+def order_list(request):
+    orders = Order.objects.filter(user=request.user)
+    return render(request, 'orders/order_list.html', {'orders': orders})  
