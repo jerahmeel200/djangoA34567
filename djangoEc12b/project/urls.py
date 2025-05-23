91be6acb67ee5e153af43898e9f7737c3f236bd5
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-05-23 21:03:06 +0100

    feat: add orders_list html template and modify other templates

--- djangoEc12b/project/urls.py
+++ djangoEc12b/project/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/products/', include('products.urls')),
-    # path('api/orders/', include('orders.urls')),
+    path('api/orders/', include('orders.urls')),
     path('api/cart/', include('cart.urls')),
     # path('api/payments/', include('payments.urls')),
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
