8a11bd95f094974cdcc39de079225c3887e0558a
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-05-23 19:46:44 +0100

    feat:Implement orders model

--- djangoEc12b/orders/models.py
+++ djangoEc12b/orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
